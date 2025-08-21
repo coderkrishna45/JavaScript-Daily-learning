793b9ca89cc59ae1b54d2ddbcee7002073320f4a
coderkrishna45 <147535375+coderkrishna45@users.noreply.github.com>
2025-08-21 15:43:34 +0530

    Add files via upload

---
+++ day1/day1.js
@@ -0,0 +1,36 @@
+// let myName="Bal Krishan Kumar";
+// console.log(myName);
+
+// let a=8;
+// let b=9;
+// let sum=a+b;
+// console.log(sum);
+
+// let a=prompt("Enter a  number :");
+//  let num1=parseInt(a);
+// let b=prompt("Enter another number:");
+//  let num2=parseInt(b);
+// let sum=num1+num2;
+// console.log(sum);
+
+//     let a=prompt("Enter a  number A :");
+//      let num1=parseInt(a);
+//     let b=prompt("Enter another number B: ");
+//      let num2=parseInt(b);
+//   console.log(" the new A is : ", num1+num2-num1);//concatenation  or `using $ here like ${  }`
+//   console.log(" The new B is : " ,num2+num1-num2);
+
+
+// let a=prompt("Enter a number to check Even Odd :");
+//  let num1=parseInt(a);
+// //  if(num1%2==0){
+// //      console.log(`${num1} is Even Number`);
+// //  }else{
+// //      console.log(`${num1} is Odd Number`);
+// //  }
+//   (num1%2==0)?console.log(`${num1} is Even Number`):console.log(`${num1} is Odd Number`);
+
+// let a=prompt("Enter the temperature in farenheit:");
+//   let num1=parseFloat(a);
+//   let result=(num1-32)*5/9;
+//   console.log(`The temperature ${num1} degree farenheit is ${result.toFixed(2)} degree celcius `);
---
+++ day1/day1mini.js
@@ -0,0 +1,8 @@
+// const computerGuess = Math.floor(Math.random() * 10) + 1;
+  //let a=prompt("Enter a number to from 1 to 10 to guess :");
+ // let num2=parseInt(a);
+  //if(num2===computerGuess){
+    //  console.log("Yahoo you guessed it right :");
+  //}else{
+    //  console.log("Oopsie Try again :");
+  //}
